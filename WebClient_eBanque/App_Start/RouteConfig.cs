3a625cb95680d5186df0c80d47f6b109edf450f8
Toma <toma.brochard@gmail.com>
2019-09-19 09:14:26 +0200

    Add ASP.Net Project

---
+++ WebClient_eBanque/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebClient_eBanque
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Acceuil",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Acceuil", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebClient_eBanque/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebClient_eBanque.MvcApplication" Language="C#" %>
